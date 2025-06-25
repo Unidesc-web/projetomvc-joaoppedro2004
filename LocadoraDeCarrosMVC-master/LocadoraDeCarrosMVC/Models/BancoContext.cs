e802e2167fd4b6a4fe7e0713775f672cab3815d7
joaoppedro2004 <167557283+joaoppedro2004@users.noreply.github.com>
2025-06-25 20:55:50 -0300

    Add files via upload

---
+++ LocadoraDeCarrosMVC-master/LocadoraDeCarrosMVC/Models/BancoContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LocadoraDeCarrosMVC.Models
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options) { }
+
+        public DbSet<Carro> Carros { get; set; }
+    }
+}
---
+++ LocadoraDeCarrosMVC-master/LocadoraDeCarrosMVC/Models/Carro.cs
@@ -0,0 +1,11 @@
+﻿namespace LocadoraDeCarrosMVC.Models
+{
+    public class Carro
+    {
+        public int Id { get; set; }
+        public string? Marca { get; set; }
+        public string? Modelo { get; set; }
+        public int Ano { get; set; }
+        public decimal PrecoDiaria { get; set; }
+    }
+}
